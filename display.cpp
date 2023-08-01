3d27aabee70c3d634258fd7bd12453832073727f
Uvataropal <114973096+Uvataropal@users.noreply.github.com>
2023-08-01 01:44:34 -0700

    added display and browse
    added display and browse

--- display.cpp
+++ display.cpp
@@ -2,5 +2,14 @@
 
 void display(Book& display_book)
 {
-    
+   cout<<" Title: "<< display_book.title<<endl;
+cout<<" Author: "<< display_book.author<<endl;
+cout<<" ISBN: "<< display_book.ISBN<<endl;
+cout<<" Publication year: "<< display_book.Publication_year<<endl;
+cout<<" Genre: "<< display_book.Genre << endl;
+if (display_book.status == "t")
+{cout<<" status: available"<<endl;}
+else
+   {cout<<" status: reserved"<<endl;
+cout<<" Due date: "<< display_book.due_date<<endl;} 
 }
