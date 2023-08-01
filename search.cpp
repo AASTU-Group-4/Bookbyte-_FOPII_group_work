04db29403be9afbb08de39fd8022c7ec8745d4de
Yeabsiramichael5 <126076921+Yeabsiramichael5@users.noreply.github.com>
2023-08-01 11:03:58 +0300

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -2,5 +2,18 @@
 
 void search(int& num, Book books[])
 {
-    
+ string isbn;
+    bool found = false;
+    cout << "Enter the ISBN of the book you want to search for: ";
+    cin >> isbn;
+    for (int i = 0; i < num; i++) {
+        if (books[i].ISBN == isbn) {
+            display(books[i]);
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        cout << "Could not find any book with ISBN " << isbn << endl;
+    }   
 }
