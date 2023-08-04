ec3942e8217dd509a5c6ebf56b09483a39229733
Uvataropal <114973096+Uvataropal@users.noreply.github.com>
2023-08-04 05:37:03 -0700

    updated display function

--- display.cpp
+++ display.cpp
@@ -2,14 +2,14 @@
 
 void display(Book& display_book)
 {
-   cout<<" Title: "<< display_book.title<<endl;
-cout<<" Author: "<< display_book.author<<endl;
-cout<<" ISBN: "<< display_book.ISBN<<endl;
-cout<<" Publication year: "<< display_book.Publication_year<<endl;
-cout<<" Genre: "<< display_book.Genre << endl;
-if (display_book.status == "t")
-{cout<<" status: available"<<endl;}
-else
-   {cout<<" status: reserved"<<endl;
-cout<<" Due date: "<< display_book.due_date<<endl;} 
+   cout << "\tTitle:\t" << display_book.title << endl;
+    cout << "\tAuthor:\t"<< display_book.author << endl;
+    cout << "\tISBN:\t" << display_book.ISBN << endl;
+    cout << "\tPublication year:\t" << display_book.Publication_year<<endl;
+    cout << "\tGenre:\t" << display_book.Genre << endl;
+    cout << "\tAvailable:\t" << display_book.status << endl;
+    if(display_book.status=="false")
+    {
+        cout <<"\tDue date:\t" << display_book.due_date << endl;
+    }  
 }
