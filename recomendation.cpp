06fac576427d106dd00d2a78af4cb95f61e6f13d
DeepBlue <jared21h25@gmail.com>
2023-08-04 16:16:48 +0300

    Update recommendation fubction

--- recomendation.cpp
+++ recomendation.cpp
@@ -1,12 +1,19 @@
-#include "header.h"
+#include "header.hpp"
 
 void reccomendation(int& num, Book books[])
 {
     srand(time(0));
     
     // Generate a random index between 0 and num-1
-    int randomIndex = rand() % num;
-    
-    // Display the randomly chosen book
-    display(books[randomIndex]);
+    if(num > 0)
+    {
+        int randomIndex = rand() % num;
+        cout << "Book " << randomIndex+1 << endl;
+        // Display the randomly chosen book
+        display(books[randomIndex]);
+    }
+    else
+    {
+        cout << "There are no books" << endl;
+    }
 }
